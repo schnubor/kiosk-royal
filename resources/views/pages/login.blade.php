a221cad042a4ac96bd8f3209adb7f00458d986a7
Christian <christian@styla.com>
2015-06-09 18:39:28 +0200

    added back home button

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -9,18 +9,25 @@ Login
         <div class="icon text-center">
             <div class="icon fa fa-unlock-alt" style="font-size: 72px; margin: 70px 0;"></div>
         </div>
-        <div class="col-md-4 col-md-offset-4 well">
-            {!! Form::open(['route' => 'login', 'method' => 'POST', 'style' => 'margin-bottom: 0;']) !!}
-                <div class="form-group">
-                    {!! Form::text('username', Input::old('username'), ['class' => 'form-control', 'placeholder' => 'Username', 'required' => 'required']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password', 'required' => 'required']) !!}
-                </div>
-                <div class="text-center">
-                    {!! Form::submit('Sign in', ['class' => 'btn btn-primary pull-right']) !!}
-                </div>
-            {!! Form::close() !!}
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 well">
+                {!! Form::open(['route' => 'login', 'method' => 'POST', 'style' => 'margin-bottom: 0;']) !!}
+                    <div class="form-group">
+                        {!! Form::text('username', Input::old('username'), ['class' => 'form-control', 'placeholder' => 'Username', 'required' => 'required']) !!}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password', 'required' => 'required']) !!}
+                    </div>
+                    <div class="text-center">
+                        {!! Form::submit('Sign in', ['class' => 'btn btn-primary pull-right']) !!}
+                    </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+        <div class="row text-center">
+            <small >
+                <a href="route('home')" style="color: #ddd;">Back to home page.</a>
+            </small>
         </div>
     </div>
 @endsection
