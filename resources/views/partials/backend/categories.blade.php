a0a0fdc1a79f0480b1a9bad153d8b2c2c4be47e9
Christian <christian@styla.com>
2015-06-18 23:05:26 +0200

    added partials

---
+++ resources/views/partials/backend/categories.blade.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong>Categories</strong>
+            </div>
+            <div class="panel-body">
+                categories
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/backend/images.blade.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong>images</strong>
+            </div>
+            <div class="panel-body">
+            images
+            </div>
+        </div>
+    </div>
+</div>
