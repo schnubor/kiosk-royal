f131755a988487dc0b11898abaf45a4e88174b71
Christian Korndörfer <info@chko.org>
2015-07-01 01:10:38 +0200

    images full width

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -31,18 +31,20 @@
             @else
                 <section class="category" style="color: {{ $category->color }}; background-color: white;">
             @endif
-                <h2>{{ $category->title }}</h2>
-                @if(!empty($category->projects))
-                    @foreach($category->projects as $project)
-                        <section class="project" style="background-color: {{ $project->bgcolor }}; color: {{ $project->color }};">
-                            <h3>{{ $project->title }}</h3>
-                            <h4>{{ $project->description }}</h4>
-                            @foreach($project->images as $image)
-                                <img src="/uploads/{{ $image->filename }}" alt="{{ $image->project->title }}" class="project-image">
-                            @endforeach
-                        </section>
-                    @endforeach
-                @endif
+                <div class="container1280">
+                    <h2>{{ $category->title }}</h2>
+                    @if(!empty($category->projects))
+                        @foreach($category->projects as $project)
+                            <section class="project" style="background-color: {{ $project->bgcolor }}; color: {{ $project->color }};">
+                                <h3>{{ $project->title }}</h3>
+                                <h4>{{ $project->description }}</h4>
+                                @foreach($project->images as $image)
+                                    <img src="/uploads/{{ $image->filename }}" alt="{{ $image->project->title }}" class="project-image">
+                                @endforeach
+                            </section>
+                        @endforeach
+                    @endif
+                </div>
             </section>
         @endforeach
     @endif
