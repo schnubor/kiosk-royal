226e181b6b1f03b7f29d1890e98893bd2462d021
schnubor <info@chko.org>
2015-07-03 17:13:30 +0200

    DOM update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -31,20 +31,20 @@
             @else
                 <section class="category" style="color: {{ $category->color }}; background-color: white;">
             @endif
-                <div class="container1280">
-                    <h2>{{ $category->title }}</h2>
-                    @if(!empty($category->projects))
-                        @foreach($category->projects as $project)
-                            <section class="project" style="background-color: {{ $project->bgcolor }}; color: {{ $project->color }};">
+                <h2>{{ $category->title }}</h2>
+                @if(!empty($category->projects))
+                    @foreach($category->projects as $project)
+                        <section class="project" style="background-color: {{ $project->bgcolor }}; color: {{ $project->color }};">
+                            <div class="container1280">
                                 <h3>{{ $project->title }}</h3>
                                 <h4>{{ $project->description }}</h4>
                                 @foreach($project->images as $image)
                                     <img src="/uploads/{{ $image->filename }}" alt="{{ $image->project->title }}" class="project-image">
                                 @endforeach
-                            </section>
-                        @endforeach
-                    @endif
-                </div>
+                            </div>
+                        </section>
+                    @endforeach
+                @endif
             </section>
         @endforeach
     @endif
