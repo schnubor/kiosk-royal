6baba94315cf4eb2e6520a6e84ceb145c4342e58
schnubor <info@chko.org>
2015-07-11 13:56:34 +0200

    more responsiveness

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,3 +3,27 @@
 <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,500' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="{{ elixir("css/all.css") }}">
 <link rel="stylesheet" href="/css/vendor/font-awesome.min.css">
+
+{{-- General stuff --}}
+<meta charset="utf-8">
+<meta name="description" content="Kiosk Royal are Carsten Aermes and Hanna Zeckau who studied visual communication at Berlin's finest art school KHB. Our work focuses both on illustration and graphic design – such as editorial, logo, web and book design.">
+<meta name="viewport" content="width=device-width,user-scalable=no">
+<meta name="keywords" content="grafik, illustration, portfolio, visual communication, berlin">
+<meta name="robots" content="all">
+<meta name="robots" content="noodp, noydir">
+
+{{-- Facebook --}}
+<meta property="og:image" content="http://www.kiosk-royal.com/images/fb.png"> 
+<meta property="og:type" content="website"> 
+<meta property="og:title" content="Kiosk Royal - Grafik und Illustration aus Berlin"> 
+<meta property="og:description" content="Kiosk Royal are Carsten Aermes and Hanna Zeckau who studied visual communication at Berlin's finest art school KHB. Our work focuses both on illustration and graphic design – such as editorial, logo, web and book design."> 
+<meta property="og:url" content="http://www.kiosk-royal.com"> 
+<meta property="og:site_name" content="kiosk-royal.com"> 
+
+{{-- Twitter Meta Data --}}
+<meta name="twitter:card" content="summary"> 
+<meta name="twitter:url" content="http://www.kiosk-royal.com"> 
+<meta name="twitter:title" content="Kiosk Royal - Grafik und Illustration aus Berlin"> 
+<meta name="twitter:description" content="Kiosk Royal are Carsten Aermes and Hanna Zeckau who studied visual communication at Berlin's finest art school KHB. Our work focuses both on illustration and graphic design – such as editorial, logo, web and book design."> 
+<meta name="twitter:image" content="http://www.kiosk-royal.com/images/fb.png"> 
+<meta itemprop="image" content="http://www.kiosk-royal.com/images/fb.jpg">
