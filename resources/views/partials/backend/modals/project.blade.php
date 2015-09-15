f31e74448552b1d8546ff149548c04dc47eab85d
Christian <christian@styla.com>
2015-09-15 10:58:20 +0200

    fixed bug editing a project position

--- resources/views/partials/backend/modals/project.blade.php
+++ resources/views/partials/backend/modals/project.blade.php
@@ -19,7 +19,7 @@
                     {!! Form::label('position') !!}
                     <select name="position" class="form-control js-position">
                         <option disabled selected>Choose position</option>
-                        @for($i = 1; $i <= $categories->count()+1; $i++)
+                        @for($i = 1; $i <= $projects->count(); $i++)
                             <option value="{{ $i }}">Position {{ $i }}</option>
                         @endfor
                     </select>
